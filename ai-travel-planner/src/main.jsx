dab084cc6f35e02b74fb425820d630949cbb0200
Dinuth Diruksha <dinuthk69@gmail.com>
2025-01-08 00:26:51 +0530

    Revert "half"
    This reverts commit 6c54f5ce69bfb621a6a709dedf12be57d14084f0.

--- ai-travel-planner/src/main.jsx
+++ ai-travel-planner/src/main.jsx
@@ -3,23 +3,16 @@ import ReactDOM from 'react-dom/client' // Corrected 'ReactDom' to 'ReactDOM'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import CreateTrip from './create-trip/index.jsx'
-import Header from './components/ui/custom/Header'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-  },
-  {
-    path: '/create-trip',
-    element: <CreateTrip />,
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
