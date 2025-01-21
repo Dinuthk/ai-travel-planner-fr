fe0cfbf9f798e764cf678f5b6ae34e29b0b6d92b
lahiru <lahirunisaluor123@gmail.com>
2025-01-21 22:54:14 +0530

    "create trip" navigation created

--- ai-travel-planner/src/components/custom/Hero.jsx
+++ ai-travel-planner/src/components/custom/Hero.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import {Link} from 'react-router-dom'
 
 const Hero = () => {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
       <h1
-      className='font-extrabold text-[50px] text-center mt-16'>
+        className='font-extrabold text-[50px] text-center mt-16'>
         <span className='text-[#f56551]'>Discover Your Nexy Adventure with AI:</span><p>Personalized Itineraries at your Fingertips</p></h1>
-        <p className='text-xl text-gray-500 text-center'>Your personal trip planner Bla Bla Bla...</p>
+      <p className='text-xl text-gray-500 text-center'>Your personal trip planner Bla Bla Bla...</p>
 
+      <Link to={'/create-trip'}>
         <Button>Get Started, It's Free</Button>
+      </Link>
+
     </div>
   )
 }
