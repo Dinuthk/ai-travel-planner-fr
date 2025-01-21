82ddb66ad787073a0a85c59596c1767dce3aa2c5
lahiru <lahirunisaluor123@gmail.com>
2025-01-21 23:11:27 +0530

    start create trip info form

--- ai-travel-planner/src/components/custom/Hero.jsx
+++ ai-travel-planner/src/components/custom/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
       <h1
-        className='font-extrabold text-[50px] text-center mt-16'>
+        className='font-bold text-[50px] text-center mt-16'>
         <span className='text-[#f56551]'>Discover Your Nexy Adventure with AI:</span><p>Personalized Itineraries at your Fingertips</p></h1>
       <p className='text-xl text-gray-500 text-center'>Your personal trip planner Bla Bla Bla...</p>
 
